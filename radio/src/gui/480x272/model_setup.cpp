efd959b0474c322373bdef847335c435a5d9e9f7
Herman <herman@ccme.nl>
2017-02-20 09:51:48 +0100

    Reverting "fix"

--- radio/src/gui/480x272/model_setup.cpp
+++ radio/src/gui/480x272/model_setup.cpp
@@ -998,14 +998,9 @@ bool menuModelFailsafe(event_t event)
       lcdDrawSolidFilledRect(xChannel, y+4, lenChannel, 5, TEXT_COLOR);
       lcdDrawSolidFilledRect(xFailsafe, y+9, lenFailsafe, 5, ALARM_COLOR);
 
-      ch++;
-	  
-      if (ch >= NUM_CHANNELS(g_moduleIdx))
+      if (++ch >= NUM_CHANNELS(g_moduleIdx))
         break;
     }
-
-	if (ch >= NUM_CHANNELS(g_moduleIdx))
-      break;
   }
 
   return true;
